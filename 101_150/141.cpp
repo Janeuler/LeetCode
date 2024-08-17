bd2f6d4255bafb1aa7fed6f426596cfe956a70a4
janeuler <janeuler@outlook.com>
2024-08-17 13:58:18 +0800

    循环链表的判断

--- 101_150/141.cpp
+++ 101_150/141.cpp
@@ -1,3 +1,41 @@
 //
 // Created by janeuler on 2024/8/17.
 //
+# include <cstddef>
+# include <vector>
+# include <algorithm>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        //     std::vector<ListNode *> v;
+        //
+        //     ListNode *p = head;
+        //     while (p != nullptr) {
+        //         ListNode *tar = p;
+        //         if (std::find(v.begin(), v.end(), tar) == v.end()) {
+        //             v.push_back(tar);
+        //         } else {
+        //             return true;
+        //         }
+        //         p = p->next;
+        //     }
+        //     return false;
+        // 快慢指针法
+        ListNode *fast = head, *slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+};
