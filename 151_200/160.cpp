19d8baa9fcc7943fd5e8d226b4f0c9623c12216c
janeuler <janeuler@outlook.com>
2024-08-30 22:47:53 +0800

    chore: Refactor ListNode struct and Solution class in 160.cpp

--- 151_200/160.cpp
+++ 151_200/160.cpp
@@ -1,3 +1,4 @@
+#include <unordered_set>
 #include <cstddef>
 
 struct ListNode
@@ -10,12 +11,51 @@ struct ListNode
     }
 };
 
+// class Solution
+// {
+// public:
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
+//     {
+//         std::unordered_set<ListNode *> set;
+//         ListNode *pA = headA;
+//         while (pA != nullptr)
+//         {
+//             set.insert(pA);
+//             pA = pA->next;
+//         }
+
+//         ListNode *pB = headB;
+//         while (pB != nullptr)
+//         {
+//             if (set.find(pB) != set.end())
+//             {
+//                 return pB;
+//             }
+//             pB = pB->next;
+//         }
+//         return NULL;
+//     }
+// };
+
+// 如果同时pa 和 pb为nullptr，怎么说明不相交
+
 class Solution
 {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB)
     {
-        
+        ListNode *p1, *p2;
+        p1 = headA;
+        p2 = headB;
 
+        if (!p1 || !p2)
+            return NULL;
+
+        while (p1 != p2)
+        {
+            p1 = p1 ? p1->next : headB;
+            p2 = p2 ? p2->next : headA;
+        }
+        return p1;
     }
 };
