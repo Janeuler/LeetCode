c83d3ca9a2bc3adddf70cdddd8dad4421628e84f
janeuler <janeuler@outlook.com>
2024-08-31 08:48:38 +0800

    findPeakElement

---
+++ 151_200/162.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+class Solution
+{
+public:
+    int findPeakElement(std::vector<int> &nums)
+    {
+        int l = 0, r = nums.size() - 1;
+        int mid;
+        while (l < r)
+        {
+            mid = l + r + (r - l) / 2;
+            if (nums[mid] < nums[mid + 1])
+            {
+                l = mid + 1;
+            }
+            else
+                r = mid;
+        }
+        return nums[mid]
+    }
+};
